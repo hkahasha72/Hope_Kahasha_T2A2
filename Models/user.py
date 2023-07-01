6a4772c1b891b8bb196a1b1f65c091404635094c
hopekahasha1 <14479@coderacademy.edu.au>
2023-07-01 19:37:34 +1000

    intitial commit

---
+++ Models/user.py
@@ -0,0 +1,8 @@
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(100))
